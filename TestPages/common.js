605c2b00edc61034d67643d8edb5eab6c2326dab
itstake <bexco2010@gmail.com>
2015-05-31 17:43:55 +0900

    MAI 2.1 체인지로그 추가

--- TestPages/common.js
+++ TestPages/common.js
@@ -1,8 +1,14 @@
 $(function() {
 	$(".container-min-height").height(0);
-
+	window.onresize = function(event) {
+    if(document.documentElement.clientWidth < 700) {
+		$(".container").width("100%");
+	} else {
+		$(".container").width("60%");
+	}
+	};
 	$(window).scroll(function() {
-		if($(window).scrollTop() < 300) {
+		if($(window).scrollTop() < 360) {
 			$(".main-slider > div").height(423);
 			$(".container-min-height").height(0);
 			$(".main-slider > div").css("position","static");
@@ -12,27 +18,9 @@ $(function() {
 				}
 			});
 		} else {
-			$(".main-slider > div").height(70);
-			$(".container-min-height").height(370);
+			$(".main-slider > div").height(60);
+			$(".container-min-height").height(623);
 			$(".main-slider > div").css("position","fixed");
 		}
-	});
-
-	$(".main-menu li").click(function(e) {
-		e.preventDefault();
-			var index = $(this).index();
-		$(".button").each(function(inx, element) {
-			if(inx == index) {
-				$(".button").eq(inx).css("text-decoration","underline");
-				var classtorage = $(this).attr("data-class");
-				console.log(classtorage);
-				$("."+classtorage+"").css("display","block");
-			} else {
-				$(".button").eq(inx).css("text-decoration","none");
-				var classtorage = $(this).attr("data-class");
-				$("."+classtorage+"").css("display","none");
-			}
-		});
-
 	});
 })
